edddc5015e4ae2894a2d386af7270af6d5c71ea4
stephaniejchan8 <stephaniejchan8@gmail.com>
2022-04-29 17:23:15 +1000

    creating expense tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Concert tickets',
+      amount: 94.12,
+      date: new Date(2021, 4, 20),
+    },
+    {
+      id: 'e2',
+      title: 'Couch',
+      amount: 799.49,
+      date: new Date(2021, 8, 11),
+    },
+    {
+      id: 'e3',
+      title: 'RACV',
+      amount: 294.67,
+      date: new Date(2021, 12, 2),
+    },
+    {
+      id: 'e4',
+      title: 'Carpet',
+      amount: 450,
+      date: new Date(2022, 3, 3),
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Hello world!</h2>
+      <p>Titi is a gym junkie.</p>
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
