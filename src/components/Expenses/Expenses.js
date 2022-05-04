142e659db960c781b1664713c27e4b286bf90aa6
stephaniejchan8 <stephaniejchan8@gmail.com>
2022-05-04 11:37:03 +1000

    added expense filter, removed demo button

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,27 +1,38 @@
+import {useState} from 'react';
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 function Expenses(props) {
+  const [enteredYear, setEnteredYear] = useState('2022');
+  const filterYearHandler = (filterYear) => {
+    setEnteredYear(filterYear);
+  }
+
   return (
-    <Card className='expenses'>
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date} />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date} />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date} />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date} />
-    </Card>
+    <div>
+      <Card className='expenses'>
+      <ExpensesFilter selectedYear={enteredYear} onFilterYear={filterYearHandler}/>
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date} />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date} />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date} />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date} />
+      </Card>
+
+    </div>
   );
 }
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -56,7 +56,7 @@ const ExpenseForm = (props) => {
         </div>
         <div className='new-expense__control'>
           <label>Date</label>
-          <input type="date" min='2021-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
+          <input type="date" min='2020-01-01' max='2022-12-31' value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className='new-expense__actions'>
