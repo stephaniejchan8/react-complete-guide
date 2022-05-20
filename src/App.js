df8a113de860c7525e2e0624518190ccf2c5ad58
stephaniejchan8 <stephaniejchan8@gmail.com>
2022-05-20 11:44:58 +1000

    practising git branches and repos

--- src/App.js
+++ src/App.js
@@ -1,47 +1,24 @@
-import { useState } from "react";
-import Expenses from "./components/Expenses/Expenses";
-import NewExpense from "./components/NewExpense/NewExpense";
+import React, { useState } from 'react';
+
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Concert tickets',
-      amount: 94.12,
-      date: new Date(2021, 4, 20),
-    },
-    {
-      id: 'e2',
-      title: 'Couch',
-      amount: 799.49,
-      date: new Date(2021, 8, 11),
-    },
-    {
-      id: 'e3',
-      title: 'RACV',
-      amount: 294.67,
-      date: new Date(2021, 12, 2),
-    },
-    {
-      id: 'e4',
-      title: 'Carpet',
-      amount: 450,
-      date: new Date(2022, 3, 3),
-    },
-  ];
+  const [usersList, setUsersList] = useState([]);
 
-  const [expensesList, setExpensesList] = useState(expenses);
-
-  const addExpenseHandler = expense => {
-    setExpensesList(prevExpenses => {
-      return [expense, ...prevExpenses];
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
   };
 
   return (
     <div>
-      <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expensesList} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,9 @@
-import './Card.css';
+import React from 'react';
 
-function Card(props) {
-  const classes = 'card ' + props.className;
-  return (
-    <div className={classes}>{props.children}</div>
-  );
-}
+import classes from './Card.module.css';
+
+const Card = (props) => {
+  return <div className={`${classes.card} ${props.className}`}>{props.children}</div>;
+};
 
 export default Card;
