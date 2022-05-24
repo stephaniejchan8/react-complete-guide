4dbcad5b122149790c2ff12c262503346866374f
stephaniejchan8 <93897509+stephaniejchan8@users.noreply.github.com>
2022-05-24 13:23:10 +1000

    context provider component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,9 @@
-import React, { useState, useEffect, useReducer } from 'react';
+import React, { useState, useEffect, useReducer,useContext } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import AuthContext from '../../store/auth-context';
 
 const Login = (props) => {
   // const [enteredEmail, setEnteredEmail] = useState('');
@@ -10,6 +11,7 @@ const Login = (props) => {
   // const [enteredPassword, setEnteredPassword] = useState('');
   // const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
+  const authCtx = useContext(AuthContext);
 
   // useEffect(() => {
   //   console.log('EFFECT RUNNING');
@@ -101,7 +103,7 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value);
+    authCtx.onLogin(emailState.value, passwordState.value);
   };
 
   return (
--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -1,32 +1,32 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
 const Navigation = () => {
-  const ctx = useContext(AuthContext);
+  const authCtx = useContext(AuthContext);
 
   return (
-        <nav className={classes.nav}>
-          <ul>
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Users</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <a href="/">Admin</a>
-              </li>
-            )}
-            {ctx.isLoggedIn && (
-              <li>
-                <button onClick={ctx.onLogout}>Logout</button>
-              </li>
-            )}
-          </ul>
-        </nav>
-      );
+    <nav className={classes.nav}>
+      <ul>
+        {authCtx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {authCtx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {authCtx.isLoggedIn && (
+          <li>
+            <button onClick={authCtx.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navigation;
